5a059cf2032c88947bc7f00acac8ef68a4c09f81
Roman <romanvasylyna888@gmail.com>
2020-06-04 18:58:19 +0300

    Added First Test Task

--- codewars.js
+++ codewars.js
@@ -26,3 +26,54 @@ return `${names[0]}, ${names[1]} and ${names[2]} like this`;
 return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
 }
 }
+
+
+Ex2
+(https://www.codewars.com/kata/52774a314c2333f0a7000688/train/javascript)
+Write a function called that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+
+Solution: (Passed: 89, Failed: 30)
+function validParentheses(parens){
+let test = /\(+\)+/g.test(parens);
+let test2 = /\(+\(+/g.test(parens);
+let test3 = /\(+\(+\s/g.test(parens);
+let test4 = /\s\(+\(+\s/g.test(parens);
+if(test || test2 || test3 || test4) {
+return true;
+}
+return false;
+}
+
+
+/*
+Тестовое задание на стажировку :
+Дан массив, состоящий из двух строк, они обязательно должны быть одинаковой длины.
+Написать функцию, которая находит символы, что отличаются и возвращает их количество
+*/
+
+function checkStr(strArr) {
+let str1 = strArr[0].split('');
+let str2 = strArr[1].split('');
+if(str1.length != str2.length) {
+return 'Length is different!';
+} else {
+return runLoop(str1, str2);
+}
+}
+
+function runLoop(arr1, arr2) {
+let newArr = [];
+for(let i = 0; i <arr1.length; i ++) {
+if(arr1[i] != arr2[i]) {
+newArr.push(arr1[i]);
+}
+}
+return newArr.length;
+}
+
+console.log(checkStr(['house','hours']));
+console.log(checkStr(['dylan','dykkn']));
